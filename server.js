3e92c484fab6d89597d13a27e2054c3de76ec5a1
SangHynh <huynhvansang.020102@gmail.com>
2024-07-21 10:21:07 +0700

    remove unuse lib

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-// // Set up view engine
-// app.engine("hbs", hbs.engine({ extname: ".hbs" }));
-// app.set("view engine", "hbs");
-// app.set("views", path.join(__dirname, "public/views"));
-
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
